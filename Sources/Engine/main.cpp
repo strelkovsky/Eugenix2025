160a70f1a701b5c53554f604f4869c8e1e1f8b69
strelkovsky <strelkin7@gmail.com>
2025-05-18 23:50:37 +0300

    initial commit

---
+++ Sources/Engine/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <vulkan/vulkan.h>
+
+int main()
+{
+	std::cout << "Hello, Vulkan!" << std::endl;
+
+	return 0;
+}
---
+++ Sources/OpenGLSandbox/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, OpenGL Sandbox!" << std::endl;
+
+	return 0;
+}
