0283b9da309bab44f2e5e06a1f8a498be4e63b46
strelkovsky <strelkin7@gmail.com>
2025-05-19 00:18:22 +0300

    Vkinstance creation

--- Sources/Engine/main.cpp
+++ Sources/Engine/main.cpp
@@ -6,5 +6,25 @@ int main()
 {
 	std::cout << "Hello, Vulkan!" << std::endl;
 
+	VkInstance instance{ VK_NULL_HANDLE };
+
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Eugenix";
+	appInfo.applicationVersion = 1;
+	appInfo.pEngineName = "Eugenix Engine";
+	appInfo.engineVersion = 1;
+	appInfo.apiVersion = VK_MAKE_VERSION(1, 4, 0);
+
+	VkInstanceCreateInfo instanceInfo{};
+	instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instanceInfo.pApplicationInfo = &appInfo;
+
+	VkResult res = vkCreateInstance(&instanceInfo, nullptr, &instance);
+	if (res != VK_SUCCESS)
+	{
+		std::cout << "Failed to create VkInstance!" << std::endl;
+	}
+
 	return 0;
 }
