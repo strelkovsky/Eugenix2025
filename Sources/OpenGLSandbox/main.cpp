dfce4c445610eb701aef736a64c070f7994f291e
strelkovsky <strelkin7@gmail.com>
2025-05-24 02:40:04 +0300

    [OpenGLSandbox] added indexed drawing, started with DSA-stuff

--- Sources/OpenGLSandbox/main.cpp
+++ Sources/OpenGLSandbox/main.cpp
@@ -11,27 +11,39 @@
 // Sandbox headers
 #include "App/SandboxApp.h"
 
-class TriangleApp final : public Eugenix::SandboxApp
+class SquareApp final : public Eugenix::SandboxApp
 {
 protected:
 	bool OnInit() override
 	{ 
-		const float vertices[] =
+		const std::vector<float> square_vertices
 		{
 			-0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f
+			 0.5f, -0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f
 		};
 
-		glGenVertexArrays(1, &_triangleVao);
-		glBindVertexArray(_triangleVao);
+		const std::vector<uint32_t> square_elements
+		{
+			0, 1, 2, 
+			1, 2, 3
+		};
 
-		glGenBuffers(1, &_triangleVbo);
-		glBindBuffer(GL_ARRAY_BUFFER, _triangleVbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glCreateVertexArrays(1, &_squareVao);
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glCreateBuffers(1, &_squareVbo);
+		glNamedBufferData(_squareVbo, square_vertices.size() * sizeof(square_vertices), square_vertices.data(), GL_STATIC_DRAW);
+
+		glCreateBuffers(1, &_squareEbo);
+		glNamedBufferData(_squareEbo, square_elements.size() * sizeof(uint32_t), square_elements.data(), GL_STATIC_DRAW);
+
+		glVertexArrayVertexBuffer(_squareVao, 0, _squareVbo, 0, sizeof(float) * 3);
+		glVertexArrayElementBuffer(_squareVao, _squareEbo);
+
+		glVertexArrayAttribFormat(_squareVao, 0, 3, GL_FLOAT, GL_FALSE, 0);
+		glVertexArrayAttribBinding(_squareVao, 0, 0);
+		glEnableVertexArrayAttrib(_squareVao, 0);
 
 		const auto vsSourceData = Eugenix::IO::FileContent("Shaders/simple.vert");
 		const char* vsSource = vsSourceData.data();
@@ -47,11 +59,11 @@ protected:
 		glShaderSource(fragmentShader, 1, &fsSource, nullptr);
 		glCompileShader(fragmentShader);
 
-		_trianglePipeline = glCreateProgram();
-		glAttachShader(_trianglePipeline, vertexShader);
-		glAttachShader(_trianglePipeline, fragmentShader);
-		glLinkProgram(_trianglePipeline);
-		glUseProgram(_trianglePipeline);
+		_squarePipeline = glCreateProgram();
+		glAttachShader(_squarePipeline, vertexShader);
+		glAttachShader(_squarePipeline, fragmentShader);
+		glLinkProgram(_squarePipeline);
+		glUseProgram(_squarePipeline);
 
 		glDeleteShader(vertexShader);
 		glDeleteShader(fragmentShader);
@@ -64,28 +76,29 @@ protected:
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(_trianglePipeline);
+		glUseProgram(_squarePipeline);
 
-		glBindVertexArray(_triangleVao);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(_squareVao);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 	}
 	
 	void OnCleanup() override
 	{
-		glDeleteProgram(_trianglePipeline);
-		glDeleteVertexArrays(1, &_triangleVao);
-		glDeleteBuffers(1, &_triangleVbo);
+		glDeleteProgram(_squarePipeline);
+		glDeleteVertexArrays(1, &_squareVao);
+		glDeleteBuffers(1, &_squareVbo);
 	}
 
 private:
-	uint32_t _triangleVao{};
-	uint32_t _triangleVbo{};
+	uint32_t _squareVao{};
+	uint32_t _squareVbo{};
+	uint32_t _squareEbo{};
 
-	uint32_t _trianglePipeline{};
+	uint32_t _squarePipeline{};
 };
 
 int main()
 {
-	TriangleApp app;
+	SquareApp app;
 	return app.Run();
 }
