2a8dde6fdd3a5978d19e39d6f3882c44001e878f
strelkovsky <strelkin7@gmail.com>
2025-05-28 23:27:37 +0300

    [Vulkan] update camera position

--- Sources/Engine/main.cpp
+++ Sources/Engine/main.cpp
@@ -1699,7 +1699,7 @@ private:
 
 		UniformBufferObject ubo{};
 		ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(30.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-		ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 		ubo.proj = glm::perspective(glm::radians(45.0f), static_cast<float>(_swapchainExtent.width / _swapchainExtent.height), 0.1f, 10.0f);
 		ubo.proj[1][1] *= -1;
 
