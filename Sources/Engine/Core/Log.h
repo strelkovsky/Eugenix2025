b3b2e347304b22d65fb57d1834c1b3dc0090be94
strelkovsky <strelkin7@gmail.com>
2025-06-01 21:51:28 +0300

    [Common] improve logger

--- Sources/Engine/Core/Log.h
+++ Sources/Engine/Core/Log.h
@@ -12,7 +12,8 @@ namespace Eugenix
 		Info,
 		Warning,
 		Error,
-		Verbose
+		Verbose,
+		Success
 	};
 
 	enum struct LogLevel
@@ -36,13 +37,29 @@ namespace Eugenix
 			(Severity == LogSeverity::Error && CurrentLogLevel >= LogLevel::ErrorOnly) ||
 			(Severity == LogSeverity::Warning && CurrentLogLevel >= LogLevel::Warnings) ||
 			(Severity == LogSeverity::Info && CurrentLogLevel >= LogLevel::All) ||
-			(Severity == LogSeverity::Verbose && CurrentLogLevel == LogLevel::All))
+			(Severity == LogSeverity::Verbose && CurrentLogLevel == LogLevel::All) ||
+			(Severity == LogSeverity::Success && CurrentLogLevel >= LogLevel::All))
 		{
+#if defined(EUGENIX_PLATFORM_WINDOWS)
+			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+			CONSOLE_SCREEN_BUFFER_INFO info;
+			GetConsoleScreenBufferInfo(hConsole, &info);
+
+			WORD original = info.wAttributes;
+			if constexpr (Severity == LogSeverity::Error) SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+			else if constexpr (Severity == LogSeverity::Warning) SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+			else if constexpr (Severity == LogSeverity::Success) SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+#endif
+
 			if constexpr (Severity == LogSeverity::Error)   std::cerr << "[Error] ";
 			if constexpr (Severity == LogSeverity::Warning) std::cerr << "[Warn] ";
 			if constexpr (Severity == LogSeverity::Info)    std::cerr << "[Info] ";
 			if constexpr (Severity == LogSeverity::Verbose) std::cerr << "[Verbose] ";
 			((std::cerr << std::forward<Args>(args)), ...) << '\n';
+
+#if defined(EUGENIX_PLATFORM_WINDOWS)
+			SetConsoleTextAttribute(hConsole, original);
+#endif
 		}
 	}
 
@@ -50,4 +67,5 @@ namespace Eugenix
 	inline void LogWarn(auto&&... args) { LogFmt<LogSeverity::Warning>(std::forward<decltype(args)>(args)...); }
 	inline void LogError(auto&&... args) { LogFmt<LogSeverity::Error>(std::forward<decltype(args)>(args)...); }
 	inline void LogVerbose(auto&&... args) { LogFmt<LogSeverity::Verbose>(std::forward<decltype(args)>(args)...); }
+	inline void LogSuccess(auto&&... args) { LogFmt<LogSeverity::Success>(std::forward<decltype(args)>(args)...); }
 }
