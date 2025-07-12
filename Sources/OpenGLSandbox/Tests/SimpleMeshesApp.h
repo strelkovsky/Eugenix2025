cb534057a67a282145b76291bd25b9d8a6b32018
strelkovsky <strelkin7@gmail.com>
2025-07-12 16:01:16 +0300

    [OpenGLSandbox] new test added

---
+++ Sources/OpenGLSandbox/Tests/SimpleMeshesApp.h
@@ -0,0 +1,172 @@
+#pragma once
+
+#include <span>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "TestUtils.h"
+
+// Sandbox headers
+#include "App/SandboxApp.h"
+#include "Render/OpenGL/Buffer.h"
+#include "Render/OpenGL/Commands.h"
+#include "Render/OpenGL/Pipeline.h"
+#include "Render/OpenGL/VertexArray.h"
+
+struct SimpleMesh
+{
+	SimpleMesh(std::span<const float> vertices, std::span<const uint32_t> indices)
+		: indexCount{ static_cast<uint32_t>(indices.size()) }
+	{
+		_vao.Create();
+
+		_vbo.Create();
+		_vbo.Storage(Eugenix::Core::MakeData(vertices));
+
+		_ibo.Create();
+		_ibo.Storage(Eugenix::Core::MakeData(indices));
+
+		constexpr Eugenix::Render::Attribute position_attribute{ 0, 3, GL_FLOAT, GL_FALSE,  0 };
+
+		_vao.AttachVertices(_vbo, sizeof(glm::vec3));
+		_vao.AttachIndices(_ibo);
+		_vao.Attribute(position_attribute);
+	}
+
+	void RenderMesh()
+	{
+		_vao.Bind();
+		Eugenix::Render::OpenGL::Commands::DrawIndexed(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT);
+	}
+
+	void ClearMesh()
+	{
+		_vbo.Destroy();
+		_ibo.Destroy();
+		_vao.Destroy();
+
+		indexCount = 0;
+	}
+
+	~SimpleMesh()
+	{
+		ClearMesh();
+	}
+
+	uint32_t indexCount = 0;
+
+	Eugenix::Render::OpenGL::VertexArray _vao{};
+	Eugenix::Render::OpenGL::Buffer _vbo{};
+	Eugenix::Render::OpenGL::Buffer _ibo{};
+};
+
+namespace Eugenix
+{
+	class SimpleMeshesApp final : public SandboxApp
+	{
+	protected:
+		bool OnInit() override
+		{
+			CreateGeometry();
+			CreatePipelines();
+
+			glEnable(GL_DEPTH_TEST);
+
+			Render::OpenGL::Commands::Clear(0.2f, 0.0f, 0.2f);
+
+			return true;
+		}
+
+		void OnUpdate(float delteTime) override
+		{
+		}
+
+		void OnRender() override
+		{
+			Render::OpenGL::Commands::Clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			// Projection matrix
+			glm::mat4 projection = glm::perspective(45.0f, (GLfloat)Width() / (GLfloat)Height(), 0.1f, 100.0f);
+			_pipeline.Bind();
+
+			glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+			{
+				// Model matrix
+				glm::mat4 model = glm::mat4(1.0f);
+
+				model = glm::mat4(1.0f);
+				model = glm::translate(model, glm::vec3(-0.6f, 0.0f, -2.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+				model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.75f));
+				glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+				meshes[0].RenderMesh();
+
+				model = glm::mat4(1.0f);
+				model = glm::translate(model, glm::vec3(0.6f, 0.0f, -2.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+				model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.75f));
+				glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+				meshes[1].RenderMesh();
+			}
+		}
+
+	private:
+		void CreateGeometry()
+		{
+			GLfloat vertices[] = 
+			{
+				-1.0f, -1.0f, 0.0f,
+				 0.0f, -1.0f, 1.0f,
+				 1.0f, -1.0f, 0.0f,
+				 0.0f,  1.0f, 0.0f,
+			};
+
+			unsigned int indices[] =
+			{
+				0, 3, 1,
+				1, 3, 2,
+				2, 3, 0,
+				0, 1, 2,
+			};
+
+			meshes.reserve(2);
+
+			meshes.emplace_back(vertices, indices);
+			meshes.emplace_back(vertices, indices);
+		}
+
+		void CreatePipelines()
+		{
+			_pipeline.Create();
+
+			const auto vsSourceData = Eugenix::IO::FileContent("Shaders/shader.vert");
+			const char* vsSource = vsSourceData.data();
+
+			const auto fsSourceData = Eugenix::IO::FileContent("Shaders/shader.frag");
+			const char* fsSource = fsSourceData.data();
+
+			auto vertexStage = Eugenix::CreateStage(vsSource, Eugenix::Render::ShaderStageType::Vertex);
+			auto fragmentStage = Eugenix::CreateStage(fsSource, Eugenix::Render::ShaderStageType::Fragment);
+
+			_pipeline.Create();
+			_pipeline.AttachStage(vertexStage)
+				.AttachStage(fragmentStage)
+				.Build();
+
+			uniformModel = glGetUniformLocation(_pipeline.NativeHandle(), "model");
+			uniformProjection = glGetUniformLocation(_pipeline.NativeHandle(), "projection");
+		}
+
+		std::vector<SimpleMesh> meshes;
+
+		Eugenix::Render::OpenGL::Pipeline _pipeline{};
+		GLuint uniformModel;
+		GLuint uniformProjection;
+	};
+}
--- Sources/OpenGLSandbox/main.cpp
+++ Sources/OpenGLSandbox/main.cpp
@@ -1,8 +1,10 @@
 // Tests headers
 #include "Tests/SquareApp.h"
+#include "Tests/SimpleMeshesApp.h"
 
 int main()
 {
-	Eugenix::SquareApp app;
+	//Eugenix::SquareApp app;
+	Eugenix::SimpleMeshesApp app;
 	return app.Run();
 }
