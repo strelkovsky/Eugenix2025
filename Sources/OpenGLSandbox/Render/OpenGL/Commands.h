4fc6434e43bc205eb19483e25bd96c4535396620
strelkovsky <strelkin7@gmail.com>
2025-08-10 23:17:42 +0300

    mark render commands as inline funcs

--- Sources/OpenGLSandbox/Render/OpenGL/Commands.h
+++ Sources/OpenGLSandbox/Render/OpenGL/Commands.h
@@ -6,22 +6,22 @@
 
 namespace Eugenix::Render::OpenGL::Commands
 {
-	void Clear(float r, float g, float b)
+	inline void Clear(float r, float g, float b)
 	{
 		glClearColor(r, g, b, 1.0f);
 	}
 
-	void Clear(uint32_t mask)
+	inline void Clear(uint32_t mask)
 	{
 		glClear(mask);
 	}
 
-	void DrawVertices(PrimitiveType primitiveType, uint32_t verticesCount, uint32_t first = 0)
+	inline void DrawVertices(PrimitiveType primitiveType, uint32_t verticesCount, uint32_t first = 0)
 	{
 		glDrawArrays(to_opengl_type(primitiveType), first, verticesCount);
 	}
 
-	void DrawIndexed(PrimitiveType primitiveType, uint32_t indicesCount, DataType indexType)
+	inline void DrawIndexed(PrimitiveType primitiveType, uint32_t indicesCount, DataType indexType)
 	{
 		glDrawElements(to_opengl_type(primitiveType), indicesCount, to_opengl_type(indexType), nullptr);
 	}
