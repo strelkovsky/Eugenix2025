40155d414f8f6bec48f829c521961d80340ea376
strelkovsky <strelkin7@gmail.com>
2025-08-11 22:27:10 +0300

    [OpenGLSandbox] add more vertex formats

--- Sources/OpenGLSandbox/Render/Vertex.h
+++ Sources/OpenGLSandbox/Render/Vertex.h
@@ -11,7 +11,14 @@ namespace Eugenix::Render::Vertex
 {
 	struct Pos
 	{
+		glm::vec3 pos;
 
+		static constexpr uint32_t stride = sizeof(glm::vec3);
+
+		static constexpr std::array<Render::Attribute, 1> layout =
+		{
+			Render::Attribute{ /*location*/0, /*size*/3, Render::DataType::Float, false, /*offset*/0 },
+		};
 	};
 
 	struct PosColor
@@ -25,6 +32,22 @@ namespace Eugenix::Render::Vertex
 		{
 			Render::Attribute{ /*location*/0, /*size*/3, Render::DataType::Float, false, /*offset*/0 },
 			Render::Attribute{ /*location*/1, /*size*/3, Render::DataType::Float, false, /*offset*/sizeof(glm::vec3) },
+		};
+	};
+
+	struct PosNormalUV
+	{
+		glm::vec3 pos;
+		glm::vec3 normal;
+		glm::vec2 uv;
+
+		static constexpr uint32_t stride = sizeof(glm::vec3) + sizeof(glm::vec3) + sizeof(glm::vec2);
+
+		static constexpr std::array<Render::Attribute, 3> layout =
+		{
+			Render::Attribute{ /*location*/0, /*size*/3, Render::DataType::Float, false, /*offset*/0 },
+			Render::Attribute{ /*location*/1, /*size*/3, Render::DataType::Float, false, /*offset*/sizeof(glm::vec3) },
+			Render::Attribute{ /*location*/1, /*size*/3, Render::DataType::Float, false, /*offset*/sizeof(glm::vec3) + sizeof(glm::vec3) },
 		};
 	};
 } // namespace Eugenix::Render::Vertex
