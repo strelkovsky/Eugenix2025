5ef7bd3e5c078c3e3219c88952d6dfc16ea0618f
strelkovsky <strelkin7@gmail.com>
2025-08-11 16:15:58 +0300

    [OpenGLSandbox] samples ordering

--- Sources/OpenGLSandbox/main.cpp
+++ Sources/OpenGLSandbox/main.cpp
@@ -1,19 +1,20 @@
 // Tests headers
 #include "Tests/0-TriangleApp.h"
 #include "Tests/1-SquareApp.h"
+#include "Tests/2-SimpleCameraApp.h"
+#include "Tests/3-TextureApp.h"
 
-#include "Tests/SimpleCameraApp.h"
-#include "Tests/TextureApp.h"
 #include "Tests/LightingApp.h"
 
 #include "Tests/LearnOpenGLApp.h"
 
 int main()
 {
-	Eugenix::TriangleApp app;
+	//Eugenix::TriangleApp app;
 	//Eugenix::SquareApp app;
-	//Eugenix::SimpleCameraApp app;
+	Eugenix::SimpleCameraApp app;
 	//Eugenix::TextureApp app;
+	
 	//Eugenix::LightingApp app;
 	//Eugenix::LearnOpenGLApp app;
 	return app.Run();
