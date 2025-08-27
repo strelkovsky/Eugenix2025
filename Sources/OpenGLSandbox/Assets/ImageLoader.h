4acf36b946db11d9c18ca94e5300504526ff7360
strelkovsky <strelkin7@gmail.com>
2025-08-28 00:39:09 +0300

    [OpenGLSandbox] fixup ImageLoader log

--- Sources/OpenGLSandbox/Assets/ImageLoader.h
+++ Sources/OpenGLSandbox/Assets/ImageLoader.h
@@ -21,7 +21,7 @@ namespace Eugenix::Assets
 			uint8_t* raw = stbi_load(name.data(), &data.width, &data.height, &data.channels, 0);
 			if (!raw)
 			{
-				LogError("Failed to load image at path", name);
+				LogError("Failed to load image at path {}", name);
 				return {};
 			}
 
