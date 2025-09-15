2bf08948bffd7efe0fae307c9c236e7c18b84460
strelkovsky <strelkin7@gmail.com>
2025-09-16 00:04:26 +0300

    [OpenGLSandbox] added skybox class & skybox test

---
+++ Sources/OpenGLSandbox/Scene/Skybox.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include <array>
+#include <span>
+#include <string>
+
+#include "Assets/ImageLoader.h"
+#include "Render/Mesh.h"
+#include "Render/Vertex.h"
+#include "Render/OpenGL/TextureCubemap.h"
+#include "Render/OpenGL/Sampler.h"
+#include "Tests/TestUtils.h"
+
+namespace Eugenix::Scene
+{
+	class Skybox
+	{
+	public:
+		void Create(const std::array<std::string, 6> imagePaths)
+		{
+			const std::array<Eugenix::Render::Vertex::Pos, 24> vertices
+			{ {
+				//Front
+				{{ -1.0f, -1.0f, 1.0f }},
+				{ { 1.0, -1.0, 1.0 } },
+				{ { 1.0,  1.0, 1.0 } },
+				{ { -1.0, 1.0, 1.0 } },
+				//Left
+				{ { -1.0, -1.0, 1.0 } },
+				{ { -1.0, 1.0, 1.0 } },
+				{ { -1.0, 1.0, -1.0 } },
+				{ { -1.0, -1.0, -1.0 } },
+				//Right
+				{ { 1.0, -1.0, 1.0 } },
+				{ { 1.0, -1.0, -1.0 } },
+				{ { 1.0, 1.0, -1.0 } },
+				{ { 1.0, 1.0, 1.0 } },
+				//Top
+				{ { -1.0, 1.0, 1.0 } },
+				{ { 1.0, 1.0, 1.0 } },
+				{ { 1.0, 1.0, -1.0 } },
+				{ { -1.0, 1.0, -1.0 } },
+				//Bottom
+				{ { -1.0, -1.0, 1.0 } },
+				{ { -1.0, -1.0, -1.0 } },
+				{ { 1.0, -1.0, -1.0 } },
+				{ { 1.0, -1.0, 1.0 } },
+				//Back
+				{ { -1.0, -1.0, -1.0 } },
+				{ { -1.0, 1.0, -1.0 } },
+				{ { 1.0, 1.0, -1.0 } },
+				{ { 1.0, -1.0, -1.0 } },
+			} };
+
+			const std::array<uint32_t, 36> indices =
+			{
+				0, 2, 1, 3, 2, 0,
+				4, 6, 5, 7, 6, 4,
+				8, 10, 9, 11, 10, 8,
+				12, 14, 13, 15, 14, 12,
+				16, 18, 17, 19, 18, 16,
+				20, 22, 21, 23, 22, 20
+			};
+
+			skyboxMesh.Build(std::span{ vertices }, std::span{ indices });
+
+			pipeline = Eugenix::MakePipelineFromFiles("Shaders/simple_skybox.vert", "Shaders/simple_skybox.frag");
+			pipeline.SetUniform("u_cubemap", 0);
+
+			Eugenix::Assets::ImageLoader loader{};
+
+			const std::array<Eugenix::Assets::ImageData, 6> images =
+			{
+				loader.Load(imagePaths[0]),
+				loader.Load(imagePaths[1]),
+				loader.Load(imagePaths[2]),
+				loader.Load(imagePaths[3]),
+				loader.Load(imagePaths[4]),
+				loader.Load(imagePaths[5])
+			};
+			;
+			cubemap.Create();
+			cubemap.Storage(images);
+			cubemap.Update(images);
+
+			sampler.Create();
+			sampler.Parameter(Eugenix::Render::TextureParam::MinFilter, Eugenix::Render::TextureFilter::Linear);
+			sampler.Parameter(Eugenix::Render::TextureParam::MagFilter, Eugenix::Render::TextureFilter::Linear);
+			sampler.Parameter(Eugenix::Render::TextureParam::WrapS, Eugenix::Render::TextureWrapping::ClampToEdge);
+			sampler.Parameter(Eugenix::Render::TextureParam::WrapT, Eugenix::Render::TextureWrapping::ClampToEdge);
+			sampler.Parameter(Eugenix::Render::TextureParam::WrapR, Eugenix::Render::TextureWrapping::ClampToEdge);
+		}
+
+		void render(const glm::mat4& viewProjectionSkyboxMatrix)
+		{
+			cubemap.Bind();
+			sampler.Bind(0);
+
+			skyboxMesh.Bind();
+
+			pipeline.Bind();
+			pipeline.SetUniform("u_viewProjectionMatrix", viewProjectionSkyboxMatrix);
+
+			Eugenix::Render::OpenGL::Commands::DepthMask(false);
+
+			skyboxMesh.Draw();
+
+			Eugenix::Render::OpenGL::Commands::DepthMask(true);
+		}
+
+	private:
+		Eugenix::Render::Mesh skyboxMesh;
+		Eugenix::Render::OpenGL::Pipeline pipeline;
+		Eugenix::Render::OpenGL::TextureCubemap cubemap;
+		Eugenix::Render::OpenGL::Sampler sampler;
+	};
+} // namespace Eugenix::Scene
---
+++ Sources/OpenGLSandbox/Tests/8-Skybox.h
@@ -0,0 +1,157 @@
+#pragma once
+
+#include <span>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "TestUtils.h"
+
+// Sandbox headers
+#include "App/SandboxApp.h"
+#include "Render/OpenGL/Buffer.h"
+#include "Render/OpenGL/Commands.h"
+#include "Render/Mesh.h"
+#include "Render/OpenGL/Pipeline.h"
+#include "Render/OpenGL/VertexArray.h"
+#include "Render/OpenGL/OpenGLTypes.h"
+#include "Render/OpenGL/TextureCubemap.h"
+
+#include "Scene/Skybox.h"
+
+namespace Eugenix
+{
+	class SkyboxApp final : public SandboxApp
+	{
+	protected:
+		bool onInit() override
+		{
+			CreateGeometry();
+			CreatePipelines();
+
+			glEnable(GL_DEPTH_TEST);
+
+			Render::OpenGL::Commands::Clear(0.2f, 0.0f, 0.2f);
+
+			const std::array<std::string, 6> skyboxImages =
+			{
+				"Textures/Skybox/right.jpg",
+				"Textures/Skybox/left.jpg",
+				"Textures/Skybox/top.jpg",
+				"Textures/Skybox/bottom.jpg",
+				"Textures/Skybox/front.jpg",
+				"Textures/Skybox/back.jpg"
+			};
+
+			skybox.Create(skyboxImages);
+
+			_camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 2.0f, 0.1f);
+
+			return true;
+		}
+
+		void onUpdate(float deltaTime) override
+		{
+			_camera.keyControl(getKeys(), deltaTime);
+			_camera.mouseControl(getMouseButtons(), getXChange(), getYChange());
+		}
+
+		void onRender() override
+		{
+			Render::OpenGL::Commands::Viewport(0, 0, width(), height());
+			Render::OpenGL::Commands::Clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			Render::OpenGL::Commands::DepthMask(true);
+
+			glm::mat4 projection = glm::perspective(45.0f, (GLfloat)width() / (GLfloat)height(), 0.1f, 100.0f);
+
+			glm::mat4 view = glm::mat4(glm::mat3(_camera.CalculateViewMatrix()));
+			skybox.render(projection * view);
+
+			_pipeline.SetUniform("view", _camera.CalculateViewMatrix());
+			_pipeline.SetUniform("projection", projection);
+
+			_pipeline.Bind();
+
+			{
+				glm::mat4 model = glm::mat4(1.0f);
+
+				model = glm::mat4(1.0f);
+				model = glm::translate(model, glm::vec3(-0.6f, 0.0f, -3.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+				model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.75f));
+				_pipeline.SetUniform("model", model);
+				_meshes[0].Bind();
+				_meshes[0].Draw();
+
+				model = glm::mat4(1.0f);
+				model = glm::translate(model, glm::vec3(0.6f, 0.0f, -3.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+				model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.75f));
+				_pipeline.SetUniform("model", model);
+				_meshes[1].Bind();
+				_meshes[1].Draw();
+
+				model = glm::mat4(1.0f);
+				model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+				model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.75f));
+				_pipeline.SetUniform("model", model);
+				_mesh.Bind();
+				_mesh.Draw();
+			}
+		}
+
+	private:
+		void CreateGeometry()
+		{
+			const std::array<Eugenix::Render::Vertex::Pos, 4> verts =
+			{ {
+				{{-1.0f, -1.0f, 0.0f }},
+				{{ 0.0f, -1.0f, 1.0f }},
+				{{ 1.0f, -1.0f, 0.0f }},
+				{{ 0.0f,  1.0f, 0.0f }}
+			} };
+
+			const std::array<uint32_t, 12> inds
+			{
+				0, 3, 1,
+				1, 3, 2,
+				2, 3, 0,
+				0, 1, 2,
+			};
+
+			_mesh.Build(std::span{ verts }, std::span{ inds });
+
+			Render::Mesh mesh{};
+			mesh.Build(std::span{ verts }, std::span{ inds });
+
+			_meshes.push_back(mesh);
+			_meshes.push_back(mesh);
+		}
+
+		void CreatePipelines()
+		{
+			_pipeline = MakePipelineFromFiles("Shaders/simple_pos_transform.vert", "Shaders/simple_pos_transform.frag");
+		}
+
+		Eugenix::Camera _camera{};
+
+		std::vector<Render::Mesh> _meshes;
+		Render::Mesh _mesh;
+
+		Eugenix::Render::OpenGL::Pipeline _pipeline{};
+		GLuint uniformModel{};
+		GLuint uniformView{};
+		GLuint uniformProjection{};
+
+		Eugenix::Scene::Skybox skybox;
+	};
+}
