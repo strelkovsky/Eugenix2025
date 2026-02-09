bcf946aeb600b0cfd4a7b6259b13d86e3a6e410e
strelkovsky <strelkin7@gmail.com>
2026-02-09 18:59:01 +0300

    [Shared] file api update

--- Sources/Engine/IO/IO.h
+++ Sources/Engine/IO/IO.h
@@ -1,30 +1,62 @@
 #pragma once
 
+//#include <expected>
+
 #include <cassert>
+#include <filesystem>
 #include <fstream>
 #include <string_view>
 #include <vector>
 
-namespace Eugenix
+namespace Eugenix::IO
 {
-	namespace IO
-	{
-        std::vector<char> FileContent(std::string_view path, int32_t mode = 0)
+    struct File
+    {
+        File() = delete;
+
+        static std::vector<char> ReadText(const std::filesystem::path& path, int32_t mode = 0)
         {
-            std::ifstream stream(std::string(path), std::ios::ate | mode);
+            assert(std::filesystem::is_regular_file(path));
+
+            std::ifstream stream(path, std::ios::ate | mode);
             assert(stream.is_open());
 
             const auto end = stream.tellg();
             assert(end > 0);
-            const size_t size = static_cast<size_t>(end);
 
-            std::vector<char> content(size+1);
-
+            const auto size = static_cast<std::size_t>(end);
+            std::vector<char> content(size + 1);
+            
             stream.seekg(0, std::ios::beg);
-            stream.read(content.data(), size);
+            if (size > 0)
+            {
+                stream.read(content.data(), size);
+            }
             content[size] = '\0';
 
             return content;
         }
-	}
-} // namespace Eugenix
+
+        static std::vector<char> ReadBinary(const std::filesystem::path& path, int32_t mode = 0)
+        {
+            assert(std::filesystem::is_regular_file(path));
+
+            std::ifstream stream(path, std::ios::ate | mode);
+            assert(stream.is_open());
+
+            const auto end = stream.tellg();
+            assert(end > 0);
+
+            const auto size = static_cast<std::size_t>(end);
+            std::vector<char> content(size);
+
+            stream.seekg(0, std::ios::beg);
+            if (size > 0)
+            {
+                stream.read(content.data(), size);
+            }
+
+            return content;
+        }
+    };
+} // namespace Eugenix::IO
