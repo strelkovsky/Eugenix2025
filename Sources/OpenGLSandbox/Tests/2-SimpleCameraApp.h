e36bac82e632603f607b204d1b44cc0e5a7087a8
strelkovsky <strelkin7@gmail.com>
2026-03-03 00:22:28 +0300

    [OpenGLSandbox] cleanup samples

--- Sources/OpenGLSandbox/Tests/2-SimpleCameraApp.h
+++ Sources/OpenGLSandbox/Tests/2-SimpleCameraApp.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#include <span>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 #include "TestUtils.h"
 
 // Sandbox headers
@@ -16,6 +10,7 @@
 #include "Render/OpenGL/ShaderProgram.h"
 #include "Render/OpenGL/VertexArray.h"
 #include "Render/OpenGL/Pipeline.h"
+#include "Render/SharedData.h"
 #include "Render/Utils/MeshGenerator.h"
 
 namespace Eugenix
@@ -25,14 +20,16 @@ namespace Eugenix
 	protected:
 		bool onInit() override
 		{
-			CreateGeometry();
-			CreatePipelines();
-
 			Render::OpenGL::Pipeline::Enable(Render::PipelineFeature::DepthTest);
-
 			Render::OpenGL::Commands::Clear(0.2f, 0.0f, 0.2f);
 		
 			_camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 2.0f, 0.1f);
+
+			_cameraData.proj = glm::perspective(45.0f, (float)width() / (float)height(), 0.1f, 100.0f);
+
+			CreateGeometry();
+			CreateShaders();
+			CreateUBOs();
 
 			return true;
 		}
@@ -41,6 +38,8 @@ namespace Eugenix
 		{
 			_camera.keyControl(getKeys(), deltaTime);
 			_camera.mouseControl(getMouseButtons(), getXChange(), getYChange());
+
+			_cameraData.view = _camera.CalculateViewMatrix();
 		}
 
 		void onRender() override
@@ -48,41 +47,43 @@ namespace Eugenix
 			Render::OpenGL::Commands::Viewport(0, 0, width(), height());
 			Render::OpenGL::Commands::Clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-			glm::mat4 projection = glm::perspective(45.0f, (GLfloat)width() / (GLfloat)height(), 0.1f, 100.0f);
-			_program.SetUniform("view", _camera.CalculateViewMatrix());
-			_program.SetUniform("projection", projection);
+			_cameraUbo.Update(Core::MakeData(&_cameraData));
+
+			//_program.SetUniform("view", _cameraData.view);
+			//_program.SetUniform("projection", _cameraData.proj);
 
 			_program.Bind();
 			{
-				glm::mat4 model = glm::mat4(1.0f);
-
-				model = glm::mat4(1.0f);
-				model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -3.0f));
-				model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-				model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.75f));
-				_program.SetUniform("model", model);
+				_model = glm::mat4(1.0f);
+				_model = glm::translate(_model, glm::vec3(-1.0f, 0.0f, -3.0f));
+				_model = glm::rotate(_model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+				_model = glm::rotate(_model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+				_model = glm::rotate(_model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+				_model = glm::scale(_model, glm::vec3(0.5f, 0.5f, 0.75f));
+				_transformUbo.Update(Core::MakeData(&_model));
+				//_program.SetUniform("model", _model);
 				_meshes[0].Bind();
 				_meshes[0].Draw();
 
-				model = glm::mat4(1.0f);
-				model = glm::translate(model, glm::vec3(1.0f, 0.0f, -3.0f));
-				model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-				model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.75f));
-				_program.SetUniform("model", model);
+				_model = glm::mat4(1.0f);
+				_model = glm::translate(_model, glm::vec3(1.0f, 0.0f, -3.0f));
+				_model = glm::rotate(_model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+				_model = glm::rotate(_model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+				_model = glm::rotate(_model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+				_model = glm::scale(_model, glm::vec3(0.5f, 0.5f, 0.75f));
+				//_program.SetUniform("model", _model);
+				_transformUbo.Update(Core::MakeData(&_model));
 				_meshes[1].Bind();
 				_meshes[1].Draw();
 
-				model = glm::mat4(1.0f);
-				model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
-				model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-				model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-				model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.75f));
-				_program.SetUniform("model", model);
+				_model = glm::mat4(1.0f);
+				_model = glm::translate(_model, glm::vec3(0.0f, 0.0f, -3.0f));
+				_model = glm::rotate(_model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+				_model = glm::rotate(_model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+				_model = glm::rotate(_model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+				_model = glm::scale(_model, glm::vec3(0.5f, 0.5f, 0.75f));
+				//_program.SetUniform("model", _model);
+				_transformUbo.Update(Core::MakeData(&_model));
 				_mesh.Bind();
 				_mesh.Draw();
 			}
@@ -118,16 +119,40 @@ namespace Eugenix
 			_meshes.push_back(genMesh);
 		}
 
-		void CreatePipelines()
+		void CreateShaders()
 		{
-			_program = MakeProgramFromFiles("Shaders/simple_pos_transform.vert", "Shaders/simple_pos_transform.frag");
+			_program = MakeProgramFromFiles("Shaders/simple_pos_transform_ubo.vert", "Shaders/simple_pos_transform_ubo.frag");
 		}
 
-		Eugenix::Camera _camera{};
+		void CreateUBOs()
+		{
+			_cameraUbo.Create();
+			_cameraUbo.Storage(Core::MakeData(&_cameraData), GL_DYNAMIC_STORAGE_BIT);
+			_cameraUbo.Bind(Render::BufferTarget::UBO, Render::BufferBinding::Camera);
+
+			_transformUbo.Create();
+			// TODO : see GP4 (create by size, not by data)
+			_transformUbo.Storage(Core::MakeData(&_model), GL_DYNAMIC_STORAGE_BIT);
+			_transformUbo.Bind(Render::BufferTarget::UBO, Render::BufferBinding::Transform);
+
+			_materialUbo.Create();
+			_materialUbo.Storage(Core::MakeData(&_color), GL_DYNAMIC_STORAGE_BIT);
+			_materialUbo.Bind(Render::BufferTarget::UBO, Render::BufferBinding::Material);
+		}
+
+		Camera _camera{};
 
 		std::vector<Render::Mesh> _meshes;
 		Render::Mesh _mesh;
 
-		Eugenix::Render::OpenGL::ShaderProgram _program{};
+		Render::OpenGL::ShaderProgram _program{};
+
+		Render::OpenGL::Buffer _cameraUbo{};
+		Render::OpenGL::Buffer _transformUbo{};
+		Render::OpenGL::Buffer _materialUbo{};
+
+		Render::Data::Camera _cameraData{};
+		glm::mat4 _model{ 1.0f };
+		glm::vec4 _color{ 1.0f };
 	};
 }
